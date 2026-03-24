5d68bf200bb410b7e3124aead7b7de18c18aef3f
Akulkarni1 <amitskulkarni1@gmail.com>
2026-03-24 14:49:42 +0530

    Added code.

--- test/src/main/java/org/example/StreamsExample.java
+++ test/src/main/java/org/example/StreamsExample.java
@@ -1,13 +1,19 @@
 package org.example;
 
-import java.util.Arrays;
-import java.util.Random;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class StreamsExample {
     public static void main(String[] args) {
         int []arr = new int[8];
         arr =fillArr(arr.length);
         Arrays.stream(arr).forEach(System.out::println);
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7,3,4, 8, 9);
+        Set <Integer>duplicateElemenets= new HashSet();
+        List elements=list.stream().filter(x->!duplicateElemenets.add(x)).collect(Collectors.toList());
+        System.out.println(elements);
+
+
     }
 
     private static int[] fillArr(int n) {
